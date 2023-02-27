a3f2f5585ba43733eed8363a88dd8fb18efd1394
Aitor <86533955+insodel@users.noreply.github.com>
2023-02-27 18:37:09 +0100

    Add files via upload

---
+++ M03 - PROGRA/Programacion secuencial/media-de-tres-num.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace calculo_de_pesetas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduzca la cantidad de euros:");
+            double euros = Convert.ToDouble(Console.ReadLine());
+            double pesetas = euros * (double)166.3860;
+            Console.WriteLine(euros + " euros son " + pesetas);
+        }
+    }
+}
