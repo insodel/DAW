8398b5d11a89ec7be013235c547193e87982856d
Aitor <86533955+insodel@users.noreply.github.com>
2023-02-27 18:39:55 +0100

    Add files via upload

---
+++ M03 - PROGRA/Programacion secuencial/area-triangulo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace area_triangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce la base del triángulo:");
+            int baseT = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Introduce la áltura del triángulo");
+            int altura = Convert.ToInt32(Console.ReadLine());
+            int area = (baseT * altura) / 2;
+            Console.WriteLine("El área del triángulo es " + area);
+        }
+    }
+}
---
+++ M03 - PROGRA/Programacion secuencial/conversor-euro-pesetas.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace conversor_euro_pesetas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduzca la cantidad de euros:");
+            double euros = Convert.ToDouble(Console.ReadLine());
+            double pesetas = euros * (double)166;
+            Console.WriteLine(euros + " euros son " + pesetas);
+        }
+    }
+}
