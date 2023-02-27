2a05c82b92c3b015556d9ab51fd4b715f79c7056
Aitor <86533955+insodel@users.noreply.github.com>
2023-02-27 19:19:36 +0100

    Add files via upload

---
+++ M03 - PROGRA/Programacion secuencial/ConversorEurospesetasBorja.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ejercicio_prueba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduzca la cantidad de euros:");
+            const euros = Convert.ToDouble(Console.ReadLine());
+            double pesetas = euros * 166;
+            Console.WriteLine("{0} euros son {1} pesetas",euros,pesetas);
+            Console.WriteLine($"{euros} euros equivale a {pesetas} pesetas.");
+           
+        }
+    }
+}
