2dd095dd3070be9744826ef319d47447c3b0aef4
Jose Antonio Montero <123546366+Sitodine99@users.noreply.github.com>
2023-03-01 23:37:42 +0100

    Update Program.cs

--- M03 - PROGRA/Programacion secuencial/archivos .cs/Program.cs
+++ M03 - PROGRA/Programacion secuencial/archivos .cs/Program.cs
@@ -1,22 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 
-namespace Prueba
+namespace Probando
 {
-    static class Program
+    class Program
     {
-        /// <summary>
-        /// Punto de entrada principal para la aplicación.
-        /// </summary>
-        [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Console.WriteLine("Hola Profesor");
         }
     }
 }
