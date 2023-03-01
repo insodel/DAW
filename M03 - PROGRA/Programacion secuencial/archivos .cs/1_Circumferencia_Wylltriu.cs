c27e7001b17264adfe9d7bb48bd66eebbcd7bb56
Wylltriu <tudelachamorro@gmail.com>
2023-03-02 00:53:41 +0100

    Create 1_Circumferencia_Wylltriu.cs

---
+++ M03 - PROGRA/Programacion secuencial/archivos .cs/1_Circumferencia_Wylltriu.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicios_de_Ampliación
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduzca el radio:");
+            const double PI = 3.1415926535897931;
+            string r = Console.ReadLine();
+            double a = int.Parse(r) * int.Parse(r) * PI;
+            double b = 2 * PI * int.Parse(r);
+            Console.WriteLine("El área del círculo es:" + a + ", y el perímetro de la circumferencia es " + b);
+
+
+        }
+    }
+}
