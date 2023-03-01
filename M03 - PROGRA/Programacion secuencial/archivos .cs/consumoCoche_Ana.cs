5e46bda4f1d62210317ce99511f58bddd46fb111
Astarthean <asakalieu@gmail.com>
2023-03-01 17:21:07 +0100

    Ejercicios 2 - 7

---
+++ M03 - PROGRA/Programacion secuencial/archivos .cs/consumoCoche_Ana.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsumoCoche
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Realiza un programa que calcule el coste del precio de la gasolina en un viaje. 
+             El usuario introducirá los kilómetros a recorrer, el precio de la gasolina y el consumo
+             del coche en litros por cada 100 kilómetros
+
+
+            consumoCoche = 4L / 100Km
+            kilometrosRecorridos = 140Km/100
+            consumoViaje = consumoCoche * kilometrosRecorridos
+            precioGasolina = 1,38€
+            precioViaje = consumoViaje * precioGasolina
+            */
+
+            Console.WriteLine("Vamos a calcular el coste de gasolina del viaje");
+
+            Console.WriteLine("Dime cuantos kilometros vas a recorrer");
+            double kilometrosRecorridos = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Introduce el precio de la gasolina");
+            double precioGasolina = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Introduce el consumo del coche");
+            double consumoCoche = Convert.ToDouble(Console.ReadLine());
+
+            double kilometrosPasados = kilometrosRecorridos / 100;
+            double consumoViaje = consumoCoche * kilometrosPasados;
+            double precioViaje = consumoViaje * precioGasolina;
+
+            Console.WriteLine($"El viaje de {kilometrosRecorridos} kilometros te costara {precioViaje} euros");
+        }
+    }
+}
---
+++ M03 - PROGRA/Programacion secuencial/archivos .cs/mediaNumeros_Anna.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MediaNumeros
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Realiza un programa que calcule la media de tres números introducidos
+             por teclado.
+            */
+
+            Console.WriteLine("Vamos a realizar la media de 3 números");
+
+            Console.WriteLine("Dime un primer número");
+            double numero1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Dime un segundo número");
+            double numero2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Dime un tercer núemero");
+            double numero3 = Convert.ToDouble(Console.ReadLine());
+
+            double media = ((numero1 + numero2 + numero3) / 3);
+
+            Console.WriteLine("La media de los números es: " + media);
+        }
+    }
+}
