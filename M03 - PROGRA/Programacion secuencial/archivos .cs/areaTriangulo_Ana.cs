2d4de7b735f2871343c7bdd1f19ad1b02a5a5906
Astarthean <asakalieu@gmail.com>
2023-03-01 17:45:23 +0100

    Update README y Ejercicio

---
+++ M03 - PROGRA/Programacion secuencial/archivos .cs/areaTriangulo_Ana.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace AreaTriangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 
+             Realiza un programa que calcule el área de un triángulo a partir de su
+             base y su altura. El área de un triángulo es a = b * h / 2
+            */
+
+            Console.WriteLine("Vamos a convertir a calcular el área de un triángulo");
+
+            Console.WriteLine("Dime cuanto mide su base");
+            double baseTriangulo = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Dime cuanto mide su altura");
+            double altura = Convert.ToDouble(Console.ReadLine());
+
+            double area = ((baseTriangulo * altura) / 2);
+
+            Console.WriteLine($"El área del triángulo es {area}");
+        }
+    }
+}
