0b5427279f70abd41058331789567e9377de4262
Aitor <86533955+insodel@users.noreply.github.com>
2023-03-01 14:11:19 +0100

    Add files via upload

---
+++ M03 - PROGRA/Programación de arrays/archivos .cs/arrays-6-aitor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace arraysAitor1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            foreach (int i in array)
+            {
+                Console.WriteLine("Posición "+ i +" del array, cuyo valor es "+(i+5));
+
+            }
+
+        }
+    }
+}
