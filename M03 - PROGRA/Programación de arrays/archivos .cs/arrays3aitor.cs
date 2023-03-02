905ca61cdb062cf9517a278ec3eae4c951a366b3
Aitor <86533955+insodel@users.noreply.github.com>
2023-03-02 19:49:05 +0100

    Add files via upload

---
+++ M03 - PROGRA/Programación de arrays/archivos .cs/arrays3aitor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace arrays3Aitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int size = 100;
+                        
+            int[] arrayLongitud = new int[size]; //Array con la longitud de las cadenas del otro array
+            string[] arrayNombres = new string[size]; //Array que almacena los nombres
+
+            for (int i = 0; i<size; i++)
+            {
+                Console.WriteLine("Introduzca un nombre:");
+                arrayNombres[i] = Console.ReadLine();
+                arrayLongitud[i] = arrayNombres[i].Length;
+                Console.WriteLine("La longitud de "+arrayNombres[i]+" es de "+arrayLongitud[i]);
+            }
+
+
+        }
+    }
+}
