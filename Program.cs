018280b1c3b7d57f592b0f2238f260f87a4dc09d
Aren1709 <adrianantolino17@gmail.com>
2023-03-03 17:35:02 +0100

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Tablas_de_Multiplicar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Escribe un numero del 1 al 9 para ver la tabla de multiplicar correspondiente");
+
+            int factor = 0, result;
+
+            int n = int.Parse(Console.ReadLine());
+
+            if (n < 10)
+            {
+                Console.WriteLine("\nLa tabla de multiplicar del {0} es:\n", n);
+
+                while (factor < 11)
+                {
+                    result = n * factor;
+                    Console.WriteLine(n + " X " + factor + " = " + result);
+                    factor++;
+                }
+            }
+            else Console.WriteLine("El numero a multiplicar debe ser menos a 10!");
+
+
+        }
+    }
+}
