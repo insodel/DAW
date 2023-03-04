f0cc5f93e1f41f79304c388f94de6f255148b869
Aitor <86533955+insodel@users.noreply.github.com>
2023-03-04 17:46:08 +0100

    Add files via upload

---
+++ M03 - PROGRA/Programación de arrays/archivos .cs/4arraysAitor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random(); //la clase Random nos permitirá generar números aleatorios para rellenar nuestro array
+            int[] array = new int[100];
+            int apariciones = 0; //variable que contendrá el número de veces que aparece el número target en el array
+
+            for (int i = 0; i < 100; i++)
+            {
+                array[i] = rnd.Next(0,50); // los números generados aleatorios estarán en el rango 0-50 
+            }
+
+            Console.WriteLine("Indique un número para realizar la búsqueda:");
+            int target = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i < 100; i++)
+            {
+                if (target == array[i])
+                {
+                    apariciones = apariciones + 1; //cada vez que se encuentre target, se sumará uno a la cuenta de apariciones
+                    Console.WriteLine("El número " + target + " se ha encontrado en la posición " + i + ", esta es la aparición número " + apariciones + " de dicho valor.");
+                }
+                else continue;
+            }
+        }
+    }
+}
---
+++ M03 - PROGRA/Programación de arrays/archivos .cs/6arraysAitor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int n = 10; // Usamos n para definir el tamaño máximo de los arrays
+            int[] arrayUno = new int[n] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; //valores añadidos para demostrar el correcto
+            int[] arrayDos = new int[n] {2, 1, 2, 1, 2, 3, 4, 5, 2, 1}; //funcionamiento del algoritmo
+            int[] arrayFinal = new int[n];
+
+            for (int i = 0, j = 0; i < 10; i++) //usamos i y j como índices de los arrays
+            {
+                arrayFinal[i] = arrayUno[i] + arrayDos[j];
+                Console.WriteLine("La posición " + i + " del array uno tiene como valor " + arrayUno[i]);
+                Console.WriteLine("La posición " + j + " del array dos tiene como valor " + arrayDos[j]);
+                Console.WriteLine("La posición " + i + " del array final tiene como valor " + arrayFinal[i]);
+            }
+
+            Console.WriteLine("Tras las sumas, el array final es:");
+            Console.WriteLine(arrayFinal);
+        }
+    }
+}
