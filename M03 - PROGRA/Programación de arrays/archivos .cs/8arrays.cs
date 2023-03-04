f8385499f71a23d84343798c8b64ca2ef9b51230
Aitor <86533955+insodel@users.noreply.github.com>
2023-03-04 19:31:09 +0100

    Add files via upload

---
+++ M03 - PROGRA/Programación de arrays/archivos .cs/8arrays.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int n = 6; // 6 como tamaño máximo para poder usar después 1 como índice y que se muestren 5 resultados
+            Random rnd = new Random();
+            int[] array = new int[n];
+            int[] arrayInvert = new int[n];
+
+            for (int i = 1; i < n; i++)
+            {
+                array[i] = rnd.Next(0, 100);
+                Console.WriteLine("Elemento número " + i + " con valor " + array[i]);
+            }
+
+            for (int j = 1; j < n; j++)
+            {
+                arrayInvert[j] = array[n-j];
+                Console.WriteLine("Elemento invertido " + j + " con valor " + arrayInvert[j]);
+            }
+            
+        }
+    }
+}
