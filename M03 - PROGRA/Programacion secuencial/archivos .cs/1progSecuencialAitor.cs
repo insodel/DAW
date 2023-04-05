0bbbeb302a4aa7cde51455887a929bfe0ba34c62
Aitor Alava <aitor.alavaruiz@gmail.com>
2023-04-05 11:33:56 +0200

    ej1+ej5 Aitor

---
+++ M03 - PROGRA/Programacion secuencial/archivos .cs/1progSecuencialAitor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp69
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int radio = Convert.ToInt32(Console.ReadLine());
+
+            double area = Math.PI * radio * radio;
+            double perimetro = 2 * Math.PI * radio;
+
+            Console.WriteLine("El área del círculo es " + area + " y el perímetro es " + perimetro);
+        }
+    }
+}
---
+++ M03 - PROGRA/Programacion secuencial/archivos .cs/5progSecuencialAitor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp70
+{
+    class Program
+    {
+     
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduzca el valor del primer cateto:\n");
+            double primerCateto = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Introduzca el valor del primer cateto:\n");
+            double segundoCateto = Convert.ToInt32(Console.ReadLine());
+
+            double hipotenusaCuadrado = (primerCateto * primerCateto) + (segundoCateto * segundoCateto);
+            double hipotenusa = Math.Sqrt(hipotenusaCuadrado);
+
+            Console.WriteLine("La longitud de la hipotenusa es de " + hipotenusa);
+
+
+
+        }
+    }
+}
