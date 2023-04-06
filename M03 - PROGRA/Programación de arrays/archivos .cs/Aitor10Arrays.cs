f46f6f79f3fb7311ce8601ced0e8e6cf59437c2b
Aitor Alava <aitor.alavaruiz@gmail.com>
2023-04-06 11:43:11 +0200

    Create Aitor10Arrays.cs

---
+++ M03 - PROGRA/Programación de arrays/archivos .cs/Aitor10Arrays.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Ordenar_sin_Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int[] array = new int[10];
+            int[] arrayOrdenado = new int[10];
+            int n = array.Length;
+            int mayorNum;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(0, 40);
+                arrayOrdenado[i] = array[i];
+            }             
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n-1; j++)
+                {
+                    if (arrayOrdenado[i] <arrayOrdenado[j])
+                    {
+                        mayorNum = arrayOrdenado[j];
+                        arrayOrdenado[j] = arrayOrdenado[i];
+                        arrayOrdenado[i] = mayorNum;
+                    }
+                }
+            }
+            foreach (var num in array)
+                Console.Write(" " + num + " ");
+            Console.WriteLine("\n");
+            foreach (var num in arrayOrdenado)
+                Console.Write(" " + num + " ");
+        }
+    }
+}
