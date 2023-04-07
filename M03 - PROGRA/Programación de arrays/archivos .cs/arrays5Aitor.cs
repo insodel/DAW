6a244a8cfba8c2c299465f8dde2568bbc187df8d
Aitor Alava <aitor.alavaruiz@gmail.com>
2023-04-07 20:52:01 +0200

    ej5 y ej7

---
+++ M03 - PROGRA/Programación de arrays/archivos .cs/arrays5Aitor.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ConsoleApp73
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduzca el valor de n:");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] array = new int[n];
+            Random rnd = new Random();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(0, 21);
+            }
+
+            int conteoAprobados = 0;
+            int conteoSuspensos = 0;
+            int promedioApro = 0;
+            int promedioSusp = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                if (array[i] >= 10)
+                {
+                    conteoAprobados++;
+                    promedioApro += array[i];
+                }
+                else
+                {
+                    conteoSuspensos++;
+                    promedioSusp += array[i];
+                }
+            }
+            promedioSusp /= conteoSuspensos;
+            promedioApro /= conteoAprobados;
+
+            Console.WriteLine(conteoAprobados + " alumnos han aprobado. La media de sus notas es de " + promedioApro + " puntos.");
+            Console.WriteLine(conteoSuspensos + " alumnos han suspendido. La media de sus notas es de " + promedioSusp + " puntos.");
+        }
+    }
+}
---
+++ M03 - PROGRA/Programación de arrays/archivos .cs/arrays7Aitor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp74
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduzca una cantidad de euros:");
+            int eurosOg = Convert.ToInt32(Console.ReadLine());
+            int[] billetes = { 500, 200, 100, 50, 20, 10, 5 };
+            float[] monedas = { 2, 1, 0.5f, 0.2f, 0.1f, 0.05f, 0.02f, 0.01f };
+            int numBilletes = 0;
+            int resto;
+
+            for (int i = 0; i < billetes.Length; i++)
+            {
+                numBilletes = eurosOg / billetes[i] ;
+                Console.WriteLine(eurosOg + " son " + numBilletes + " billetes de " + billetes[i] + " euros.");
+            }
+
+            float numMonedas = 0;
+            for (int i = 0; i < monedas.Length; i++)
+            {
+                numMonedas = (float)eurosOg / monedas[i];
+                Console.WriteLine(eurosOg + " son " + numMonedas + " monedas de " + monedas[i] + " euros.");
+            }
+        }
+    }
+}
