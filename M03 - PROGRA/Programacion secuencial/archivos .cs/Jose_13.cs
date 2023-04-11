182ac55d3d59f5905a9ad67e09bf56b6d95f03fb
fromzerotocodder <jlecruz94@gmail.com>
2023-04-11 23:22:03 +0200

    Create Jose_13.cs

---
+++ M03 - PROGRA/Programacion secuencial/archivos .cs/Jose_13.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Ejercicio_13 /* Supongamos que tenemos un tablero de ajedrez con las casillas numeradas de la 1 a la 64 en el orden de izquierda a derecha y de arriba abajo.
+                          Realiza un programa que calcula la fila y la columna en la que se encuentra una casilla introducida por el usuario.*/
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[64];
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                array[i] = i;
+            }
+            Console.WriteLine("Introduce la casilla (entre 1 y 64) para saber su localización: ");
+            int casilla = int.Parse(Console.ReadLine());
+            int columna = 0;
+            int fila = 0;
+            if (casilla % 8 == 0)
+            {
+                columna = 8;
+                fila = casilla / 8;
+            }
+            else
+            {
+                columna = casilla % 8;
+                fila = (casilla / 8) + 1;
+            }
+            Console.WriteLine("La casilla {0} está en la fila {1} y columna {2}", casilla, fila, columna);
+        }
+    }
+}
